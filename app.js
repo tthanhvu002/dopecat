142baf34e961d2135af3c64d5f25655c827c64f6
tthanhvu002 <81304749+tthanhvu002@users.noreply.github.com>
2022-08-21 16:39:37 +0700

    pj

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 const swiper = new Swiper(".swiper", {
-    slidesPerView: 3,
-    spaceBetween: 30,
-    centeredSlides: true,
-    loop: true, 
-    autoplay: true,
-  });
+  slidesPerView: 3,
+  spaceBetween: 30,
+  centeredSlides: true,
+  loop: true,
+  autoplay: true,
+});
+
+const dropdown = document.querySelectorAll('.faq li')
+Array.from(dropdown).map(item => {
+    item.onclick = () => {
+        item.querySelector('i').classList.toggle('active')
+        item.querySelector('.sub-dropdown').classList.toggle('active')
+
+    }
+})

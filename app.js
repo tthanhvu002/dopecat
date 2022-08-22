3f09412963a4d201f264ba5d783791c4d9896ccf
tthanhvu002 <81304749+tthanhvu002@users.noreply.github.com>
2022-08-22 22:13:10 +0700

    pj

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const swiper = new Swiper(".swiper", {
   centeredSlides: true,
   loop: true,
   autoplay: true,
+  autoplay: {
+    delay: 800,
+  },
 });
 
 const dropdown = document.querySelectorAll('.faq li')
@@ -18,12 +21,22 @@ Array.from(dropdown).map(item => {
 })
 
 
-const auto = ['./img/299045514_5831842696876861_7305700469678770245_n.png','./img/298722911_597920858623474_1957435508165404036_n.png','./img/298599893_1571997493195055_411864110059032407_n.png','./img/297037200_437778098301834_7399183873352312458_n.png', './img/295710247_485164092947949_5098938862702838077_n.png','./img/295713697_457239852944185_500168885155012324_n.png',]
+const auto = ['./img/1.png','./img/2.png',
+'./img/3.png',
+
+'./img/5.png',
+'./img/6.png',
+'./img/7.png',
+'./img/8.png',
+'./img/9.png',
+
+'./img/11.png',
+'./img/12.png',]
 
 setInterval(() => {
   let index = Math.floor(Math.random() * auto.length)
   document.querySelector('.desc img').src = auto[index]
-}, 100);
+}, 200);
 
 
 document.querySelectorAll('.navbar li').forEach(item => {
